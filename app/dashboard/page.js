1ecc5a4addf7fa9f25f193aa5489ce93da2261bd
sumit <mazumdarsumit3@gmail.com>
2025-01-02 15:50:18 +0530

    add: set the header

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Dashboard() {
+  return <div></div>;
+}
+
+export default Dashboard;
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,12 +25,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          suppressHydrationWarning
+        >
+          {/* <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
